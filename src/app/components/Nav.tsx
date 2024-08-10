3559360f2f6adcdad69acd5608beac838fcdb788
Patrickodey360coder <patrickodey419@gmail.com>
2024-08-10 23:22:22 +0100

    Nav links

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Nav = () => {
   return (
     <div>
       <div className="nav-content">
-        <div className="logo"></div>
-        <div className="nav-links"></div>
+        <div className="logo">Romaan</div>
+        <div className="nav-links">
+          <Link href="#">Home</Link>
+          <Link href="#">Membership</Link>
+          <Link href="#">About Us</Link>
+          <Link href="#">Shop</Link>
+        </div>
       </div>
     </div>
   )

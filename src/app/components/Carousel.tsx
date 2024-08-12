cc99d89a36bf7a4ff341020a924c6529c05016ce
Patrickodey360coder <patrickodey419@gmail.com>
2024-08-12 21:34:12 +0100

    Swiper

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -1,54 +1,45 @@
 "use client"
 import React from 'react'
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
 import Image from 'next/image'
 import Slide_One from '../../../public/images/slider-1.jpg'
 import Slide_Two from '../../../public/images/slider-2.jpg'
 import Slide_Three from '../../../public/images/slider-3.jpg'
 import Slide_Four from '../../../public/images/slider-4.jpg'
-import Autoplay from "embla-carousel-autoplay"
-
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import { Autoplay } from 'swiper/modules';
 
 
 const Slider = () => {
   return (
-    <div className='slider'>
-      <Carousel className='w-full' 
-        plugins={
-          [
-          Autoplay({
-            delay: 3000,
-          })
-        ]}
-
-        opts={{
-          loop: true
-        }}
-      >
-        <CarouselContent className='w-full'>
-          <CarouselItem className='w-full'>
-            <Image src={Slide_One}  alt='slide one' height={1980}/>
-          </CarouselItem>
-          <CarouselItem>
-            <Image src={Slide_Two}  alt='slide two' height={1980}/>
-          </CarouselItem>
-          <CarouselItem>
-            <Image src={Slide_Three}  alt='slide three' height={1980}/>
-          </CarouselItem>
-          <CarouselItem>
-            <Image src={Slide_Four}  alt='slide Four' height={1980}/>
-          </CarouselItem>
-        </CarouselContent>
-        {/* <CarouselPrevious />
-        <CarouselNext /> */}
-      </Carousel>
-    </div>
+    <Swiper
+      className='h-[500px]'
+      spaceBetween={50}
+      slidesPerView={1}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false
+      }}
+      navigation={true}
+      modules={[Autoplay]}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide className='w-full h-full'>
+        <Image src={Slide_One}  alt='slide one' height={1980}/>
+      </SwiperSlide>
+      <SwiperSlide className='w-full'>
+        <Image src={Slide_Two}  alt='slide two' height={1980}/>
+      </SwiperSlide>
+      <SwiperSlide className='w-full'>
+        <Image src={Slide_Three}  alt='slide three' height={1980}/>
+      </SwiperSlide>
+      <SwiperSlide className='w-full'>
+        <Image src={Slide_Four}  alt='slide Four' height={1980}/>
+      </SwiperSlide>
+    </Swiper>
   )
 }
 
